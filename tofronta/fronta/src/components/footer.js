9bd5ee16c8e9cb0e9fe5c869e6214b11cd1aa5fd
GENESESHB <hassan.hbmama@gmail.com>
2025-01-02 16:42:54 +0100

    add new footer

--- tofronta/fronta/src/components/footer.js
+++ tofronta/fronta/src/components/footer.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './styles/footer.css'; // Ensure this path is correct
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
-import logo from '../assets/lg.png';
+import { faFacebookF, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import logo from '../assets/hbnb.png';
 
 const Footer = () => {
   return (
@@ -12,25 +12,25 @@ const Footer = () => {
           <div className="col-1-2">
             <img src={logo} alt="Logo" className='imf' />
             <address className="footer-address">
-              972 Mission St<br />
-              San Francisco, CA<br />
-              94103
+              3000 FES <br />
+              Fes Meknes, Morocco<br />
+              5145
             </address>
           </div>
           <div className="col-1-2">
             <ul className="social nav">
               <li className="social-item nav-item">
-                <a href="https://www.facebook.com/HolbertonSchool/" className="social-link" aria-label="Facebook" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.facebook.com/hassan.boudraa.58/" className="social-link" aria-label="Facebook" target="_blank" rel="noopener noreferrer">
                   <FontAwesomeIcon icon={faFacebookF} />
                 </a>
               </li>
               <li className="social-item nav-item">
-                <a href="https://twitter.com/holbertonschool" className="social-link" aria-label="Twitter" target="_blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faTwitter} />
+                <a href="https://www.linkedin.com/in/hassan-boudraa-se/" className="social-link" aria-label="Twitter" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faLinkedin} />
                 </a>
               </li>
               <li className="social-item nav-item">
-                <a href="https://www.instagram.com/holbertonschool/" className="social-link" aria-label="Instagram" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.instagram.com/hassan_boudraa/" className="social-link" aria-label="Instagram" target="_blank" rel="noopener noreferrer">
                   <FontAwesomeIcon icon={faInstagram} />
                 </a>
               </li>
@@ -40,7 +40,7 @@ const Footer = () => {
         <hr className="line-divider"/>
         <div className="row2">
           <div className="col-1-2">
-            <p className="footer-copyright">© 2024 Locallens.inc, made with ♥ by SE:HassanBOUDRAA.</p>
+            <p className="footer-copyright">© 2024 hb.dev, made with ♥ by SE:HassanBOUDRAA.</p>
           </div>
           <div className="col-1-2">
             <ul className="footer-nav">
